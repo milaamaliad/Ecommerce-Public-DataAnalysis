d211be7edf003d6be2564d58ef966eb5dc860a80
milaamaliad <milaamaliad@gmail.com>
2023-12-25 02:21:58 +0700

    finish

---
+++ dashboard/dashboard.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import streamlit as st
+from streamlit_lottie import st_lottie
+import urllib
+from func import DataAnalyzer, BrazilMapPlotter
+sns.set(style='dark')
+
+st.set_page_config(page_title="E-Commerce Public Data Analysis", page_icon=":tree", layout='wide')
+
+# Dataset
+datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all_df = pd.read_csv("df.csv")
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+# Geolocation Dataset
+geolocation = pd.read_csv('geolocation.csv')
+data = geolocation.drop_duplicates(subset='customer_unique_id')
+
+for col in datetime_cols:
+    all_df[col] = pd.to_datetime(all_df[col])
+
+min_date = all_df["order_approved_at"].min()
+max_date = all_df["order_approved_at"].max()
+
+# Sidebar
+with st.sidebar:
+    # Title
+    st.title("Milaamaliad")
+
+    # Logo Image
+    st.image("logo.png")
+
+    # Date Range
+    start_date, end_date = st.date_input(
+        label="Select Date Range",
+        value=[min_date, max_date],
+        min_value=min_date,
+        max_value=max_date
+    )
+
+with st.container():
+    st.subheader('E-Commerce Data Analysis')
+    st.title("Hi, I am Mila :wave:")
+
+with st.expander("learn more"):
+      st.write("[LinkedIn](https://linkedin.com/in/mila-amalia-damayanti)")
+      st.write("[GitHub](https://github.com/milaamaliad)")
+
+
+# Main
+main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
+                 (all_df["order_approved_at"] <= str(end_date))]
+
+
+function = DataAnalyzer(main_df)
+map_plot = BrazilMapPlotter(data, plt, mpimg, urllib, st)
+
+daily_orders_df = function.create_daily_orders_df()
+sum_spend_df = function.create_sum_spend_df()
+sum_order_items_df = function.create_sum_order_items_df()
+review_score, common_score = function.review_score_df()
+state, most_common_state = function.create_bystate_df()
+order_status, common_status = function.create_order_status()
+
+# Order Items
+st.subheader("What products have the largest and the lowest number of purchases?")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["product_count"].sum()
+    st.markdown(f"Total Items: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["product_count"].mean()
+    st.markdown(f"Average Items: **{avg_items}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+
+colors = ["#068DA9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Most Sold Product", loc="center", fontsize=50)
+ax[0].tick_params(axis ='y', labelsize=35)
+ax[0].tick_params(axis ='x', labelsize=30)
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Least Sold Product", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+with st.expander("See answer"):
+    st.write("The products that sell the most are furniture products and the products that sell the least are baby products")
+
+# Rating Score
+st.subheader("What is the level of buyer satisfaction with the service?")
+col1,col2 = st.columns(2)
+
+with col1:
+    avg_review_score = review_score.mean()
+    st.markdown(f"Average Review Score: **{avg_review_score}**")
+
+with col2:
+    most_common_review_score = review_score.value_counts().index[0]
+    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.barplot(x=review_score.index, 
+            y=review_score.values, 
+            order=review_score.index,
+            palette=["#068DA9" if score == common_score else "#D3D3D3" for score in review_score.index]
+            )
+
+plt.title("Rating by customers for service", fontsize=15)
+plt.xlabel("Rating")
+plt.ylabel("Count")
+plt.xticks(fontsize=12)
+st.pyplot(fig)
+
+with st.expander("See answer"):
+    st.write("Customer satisfaction with the services provided is very satisfying because the visualization that has been displayed shows that there are many customers who give a rating of 5, and a rating of 4 is the 2nd most.")
+
+# Customer Demographic
+st.subheader("Where is the geographic location that has the most customers?")
+tab1, tab2 = st.tabs([" ", " "])
+
+with tab1:
+    map_plot.plot()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+with st.expander("See Answer"):
+    st.write('According to the graph that has been created, there are more customers in the southeast and south. Other information, there are more customers in cities that are capitals (São Paulo, Rio de Janeiro, Porto Alegre, and others).')
+
+st.caption('Copyright (C) Milaamaliad 2023')
+
+
